73013526984509160862330fc4b084781d4c9683
Meg <msi32@cornell.edu>
2024-11-01 19:46:45 -0400

    Add bare welcome page

---
+++ src/app/welcome/page.js
@@ -0,0 +1,20 @@
+const Welcome = () => {
+    return (
+        <>
+            <div>
+                <div>
+                    <a>About Us</a>
+                    <a>Sign Up</a>
+                    <a>Log In</a>
+                </div>
+                <div>
+                    <img></img>
+                    <p></p>
+                    <button>Connect</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Welcome;
