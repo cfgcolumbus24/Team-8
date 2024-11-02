8f980eccf233144cadacc00f0dddebd3a13d8333
Meg <msi32@cornell.edu>
2024-11-02 02:15:37 -0400

    delete next files

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/meg/Documents/career/code-for-good/Team-8/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
