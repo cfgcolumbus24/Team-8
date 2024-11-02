57019544c99fb7392a917c69491db4397e09904d
Meg <msi32@cornell.edu>
2024-11-01 20:06:46 -0400

    Add background image and overlay

--- src/app/welcome/page.js
+++ src/app/welcome/page.js
@@ -1,19 +1,19 @@
 const Welcome = () => {
     return (
         <>
-            <div>
-                <div>
+            <div className="bg-[url('/assets/image/welcome-screen-bg.jpg')] bg-cover bg-center bg-white/60 bg-blend-overlay w-screen h-screen overflow-hidden flex flex-col">
+                <div className="flex gap-8 p-4 justify-end text-lg">
                     <a>About Us</a>
                     <a>Sign Up</a>
                     <a>Log In</a>
                 </div>
-                <div>
-                    <img></img>
-                    <p></p>
+                <div className="flex flex-col gap-8 flex-1 justify-center items-center">
+                    <img src="/assets/image/lmcc-50-years-logo.png" className="h-64 w-auto"></img>
+                    <p>Networking platform for alumni.</p>
                     <button>Connect</button>
                 </div>
             </div>
-        </>
+    </>
     );
 };
 
